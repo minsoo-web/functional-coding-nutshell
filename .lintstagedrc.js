6dbcbebbbd04357e9bb28ad0e8900288203f7519
minsoo-web <zlemzlem5656@naver.com>
2023-07-15 09:34:53 +0000

    chore: add jest package

--- .lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-const buildEslintCommand = (filenames) =>
-  `yarn lint ${filenames.map((f) => path.relative(process.cwd(), f)).join(" ")}`;
-
-module.exports = {
-  "./apps/**/*.{ts,tsx}": [buildEslintCommand],
-};
